954fe653eed2fae403eb98611ce3543aa733b6c2
firstmeet <2022742378@qq.com>
2023-10-22 23:18:13 +0800

    1

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 const { execSync } = require('child_process')
 // const fs = require('fs')
-// const gitDiffParser = require('gitdiff-parser')
+const gitDiffParser = require('gitdiff-parser').parse
 
-let diffstr = execSync('git diff ac94753c508262a43c3bd58d88c76aa65525b613 339472c12642c3e3c4f92cf02039fe62632eb085', {
+let diffstr = execSync('git diff ac94753c508262a43c3bd58d88c76aa65525b613 933b52b0209e4c8e4f04acd30cb6edfa712a2da9', {
   maxBuffer: 1024 ** 6,
-})
-console.log(diffstr.toString())
+}).toString()
+let res = gitDiffParser(diffstr)
+console.log(res[1].hunks)
