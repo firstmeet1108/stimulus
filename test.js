339472c12642c3e3c4f92cf02039fe62632eb085
firstmeet <2022742378@qq.com>
2023-10-22 22:58:30 +0800

    123

--- test.js
+++ test.js
@@ -1,50 +1,8 @@
 const { execSync } = require('child_process')
-const fs = require('fs')
-const { promisify } = require('util')
-const writeFile = promisify(fs.writeFile)
-const gitDiffParser = require('gitdiff-parser')
+// const fs = require('fs')
+// const gitDiffParser = require('gitdiff-parser')
 
-const HEAD = require('./head')
-let newCommit = ''
-let diffstr = ''
-
-try {
-  // 切换分支至subscription/main
-  execSync('git checkout remotes/subscription/main')
-  console.log('切换分支至subscription/main')
-
-  // 获取当前分支最新commit号
-  newCommit = execSync('git rev-parse HEAD').toString().replace(/\n/g, '')
-  console.log('获取当前分支最新commit号:' + newCommit)
-
-  // 判断当前分支是否有变更
-  if (HEAD === newCommit) {
-    console.log('commit号未变更 无需进行diff对比')
-
-    execSync('git checkout main')
-    console.log('返回主分支')
-
-    return
-  }
-
-  // 对指定文件进行diff对比
-  diffstr = execSync(`git diff ${HEAD} ${newCommit} -- docs/`).toString()
-  console.log('获取diff对比文本')
-  let res = gitDiffParser(diffstr)
-  console.log(res)
-
-  // 切换分支至main
-  execSync('git checkout main')
-  console.log('返回主分支')
-
-  // 更新HEAD commit号
-  writeFile('./head.js', `module.exports = ${JSON.stringify(newCommit)}`)
-    .then(() => {
-      console.log('写入成功')
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-} catch (err) {
-  console.log(err)
-}
+let diffstr = execSync('git diff ea3438b4ec784cda5b08099762396fcf38a5d97e e6bbdb35d35ebb6676d45c5d0b9e186ae586600e', {
+  maxBuffer: 1024 ** 6,
+})
+console.log(diffstr.toString())
