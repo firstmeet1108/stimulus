2c85a601981ef86ceb67c09c1920a028a28587bb
firstmeet <2022742378@qq.com>
2023-10-25 10:46:12 +0800

    为什么站点炸了？

--- test.js
+++ test.js
@@ -1,12 +1,9 @@
-const { execSync } = require('child_process')
-// const fs = require('fs')
-const gitDiffParser = require('gitdiff-parser').parse
+var Git = require('nodegit')
 
-let diffstr = execSync(
-  'git diff 3cd846c75048e7c34f77c1ed27046f2be489a7f7 80f1b950472c65ff14adf762c0133d5a29fd11d6 -- .github/workflows/',
-  {
-    maxBuffer: 1024 ** 6,
-  },
-).toString()
-let res = gitDiffParser(diffstr)
-console.log(res[0].hunks[0])
+var pathToRepo = require('path').resolve('./.git')
+
+let repo = Git.Repository.open(pathToRepo)
+
+Git.Diff.indexToIndex(repo, 'null', null).then(function (diff) {
+  console.log(diff)
+})
