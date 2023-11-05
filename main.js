bf8d651ddadc0215220551593e14cba411bb14aa
firstmeet <2022742378@qq.com>
2023-11-06 01:55:33 +0800

    1

--- main.js
+++ main.js
@@ -1,58 +1,18 @@
-const { execSync } = require('child_process')
-// const nodegit = require('nodegit')
-const fs = require('fs')
-const { promisify } = require('util')
-const writeFile = promisify(fs.writeFile)
+const { execSync, exec } = require('child_process')
+// const fs = require('fs')
+// const { promisify } = require('util')
+// const writeFile = promisify(fs.writeFile)
 
 const HEAD = require('./head')
-let newCommit = ''
-let diffstr = ''
+
 ;(async () => {
   try {
-    // 切换分支至remotes/docs/main
-    execSync('git checkout remotes/docs/main')
-    console.log('切换分支至remotes/docs/main')
-
-    // 获取当前分支最新commit号
-    newCommit = execSync('git rev-parse HEAD').toString().replace(/\n/g, '')
-    console.log('获取当前分支最新commit号:' + newCommit)
-
-    // 判断当前分支是否有变更
-    if (HEAD === newCommit) {
-      console.log('commit号未变更 无需进行diff对比')
-
-      execSync('git checkout main')
-      console.log('返回主分支')
-
-      return
-    }
-
-    // 对指定文件进行diff对比
-    diffstr = execSync(`git diff ${HEAD} ${newCommit} -- docs/`).toString()
-    console.log('获取diff对比文本')
-    console.log(diffstr)
-    // let res = gitDiffParser(diffstr)
-
-    // await nodegit.Diff.fromBuffer(diffstr, diffstr.length).then((diff) => {
-    //   console.log('获取diff对象如下')
-    //   console.log(diff)
-    // })
-    // 切换分支至main
-    execSync('git checkout main')
-    console.log('返回主分支')
-
-    // 更新HEAD commit号
-    await writeFile(
-      './head.js',
-      `module.exports = ${JSON.stringify(newCommit)}`,
-    )
-      .then(() => {
-        console.log('写入成功')
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  } catch (err) {
-    console.log(err)
+    execSync('git diff main remotes/docs/main -- docs/ > test.patch').toString()
+    execSync('git apply test.patch').toString()
+    execSync('git add ./docs').toString()
+    execSync('git commit -m "Update docs"').toString()
+    execSync('git push').toString()
+  } catch (e) {
+    console.log('Error: ', e)
   }
 })()
