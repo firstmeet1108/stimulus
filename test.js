92830c928bbf6943adbc9bb5fa0235825250c8f8
firstmeet <2022742378@qq.com>
2023-11-05 15:01:00 +0800

    1

--- test.js
+++
@@ -1,9 +0,0 @@
-var nodegit = require('nodegit')
-const path = require('path')
-// 8cbca6db3b1b2ddb384deb3dd98397d3609d25a0
-nodegit.Repository.open(path.resolve(__dirname, '.git')).then(async (repo) => {
-  let remote = await repo.getRemote('subscription')
-  await remote.connect(nodegit.Enums.DIRECTION.FETCH)
-  let references = await remote.referenceList()
-  console.log(references.length)
-})
