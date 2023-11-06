815e37467dc03a27c1b37333e4e2753dc3e3e141
firstmeet <2022742378@qq.com>
2023-11-06 13:45:05 +0800

    1

--- server/index.js
+++ server/index.js
@@ -1,15 +1,52 @@
 const Koa = require('koa')
 const cors = require('@koa/cors')
 const Router = require('koa-router')
+const bodyParser = require('koa-bodyparser')
+const nodemailer = require('nodemailer')
 
 // 创建 koa 实例
 const app = new Koa()
 
+let transporter = nodemailer.createTransport({
+  // host: 'smtp.qq.com',
+  port: 465,
+  service: 'qq',
+  secure: true, // true for 465, false for other ports
+  auth: {
+    // 发送邮件的邮箱和授权码（不是密码）
+    user: '',
+    pass: 'xxxxxx',
+  },
+})
+
+let mailOptions = {
+  from: '"firstmeet" <2022742378@qq.com>',
+  to: '2022742378@qq.com',
+  subject: 'Hello',
+  html: '<b>Hello world?</b>',
+}
 // 创建路由实例
 const router = new Router()
+app.use(bodyParser())
 
 router.post('/', async (ctx) => {
-  console.log(ctx.request.body)
+  let pushData = ctx.request.body
+  let detail = JSON.parse(pushData.payload)
+  console.log(detail.head_commit.modified)
+  let updateFiles = detail.head_commit.modified
+  updateFiles.forEach((element) => {
+    let dir = element.split('/')[0]
+    if (dir === 'docs') {
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          return console.log(error)
+        }
+        console.log('Message sent: %s', info.messageId)
+        console.log('已通知管理员更新文档')
+      })
+    }
+  })
+  ctx.body = 'OK'
 })
 
 app.use(cors())
