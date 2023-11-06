4f89da99f48ac9614dc6d8627e06de3e7c87ce78
firstmeet <2022742378@qq.com>
2023-11-06 16:11:50 +0800

    123

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const { execSync } = require('child_process')
 ;(async () => {
   try {
     execSync('git diff main remotes/docs/main -- docs/ > test.patch').toString()
-    execSync('git apply test.patch').toString()
-    execSync('git add ./docs').toString()
-    execSync('git commit -m "Update docs"').toString()
-    execSync('git push').toString()
+    // execSync('git apply test.patch').toString()
+    // execSync('git add ./docs').toString()
+    // execSync('git commit -m "Update docs"').toString()
+    // execSync('git push').toString()
   } catch (e) {
     console.log('Error: ', e)
   }
