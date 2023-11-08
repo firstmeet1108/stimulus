692ba9874bc24ce52c57a7e62714f07e1b97e8a6
firstmeet <2022742378@qq.com>
2023-11-08 11:18:26 +0800

    1

--- server/main.js
+++ server/main.js
@@ -1,18 +1,22 @@
 const { execSync } = require('child_process')
+const config = {
+  cwd: '../',
+}
 const test = () => {
   try {
     let patchStr = execSync(
-      'git diff main remotes/docs/main -- ../docs/ ',
+      'git diff main remotes/docs/main -- docs ',
+      config,
     ).toString()
     if (!patchStr) {
       console.log('无更新')
       return
     }
-    execSync('git diff main remotes/docs/main -- ../docs/ > test.patch')
-    execSync('git apply test.patch')
-    execSync('git add ../docs')
-    execSync('git commit -m "Update docs"')
-    execSync('git push')
+    execSync('git diff main remotes/docs/main -- docs > ./server/test.patch', config)
+    execSync('git apply ./server/test.patch', config)
+    execSync('git add ./docs', config)
+    execSync('git commit -m "Update docs"', config)
+    execSync('git push', config)
   } catch (e) {
     console.log('Error: ', e)
   }
