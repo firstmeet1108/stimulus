6dd08d359c5f84284c4f754bc44026589f563eb0
Coffee-kitten <1002384246@qq.com>
2023-11-22 22:47:33 +0800

    update openai-test.js

--- openai-test.js
+++ openai-test.js
@@ -3,51 +3,37 @@ const openai = new OpenAI();
 const fs = require("fs");
 const matter = require("gray-matter");
 
+
 // OpenAI API 密钥 Linux 设置方法 export OPENAI_API_KEY=''
 
-const inputFile = "00_the_origin_of_stimulus.md";
-const inputText = fs.readFileSync(inputFile, "utf-8");
+module.exports = (inputFile) => {
+  // API 请求参数
+  const markdownContent = fs.readFileSync(inputFile, "utf8");
+  const markdownFile = inputFile;
+  const { data, content } = matter(markdownContent);
 
-// API 请求参数
-const model = "gpt-3.5-turbo"; // 模型
-const messages = [
-  {
-    role: "system",
-    content:
-      "Translate the following Markdown text into Chinese and return it to me in Markdown code format. Do not translate the contents of Stimulus. Do not translate the contents of the expression '---([\s\S]+?)---'.  ", // 翻译的条件
-  },
-  { role: "user", content: inputText },
-];
-openai.chat.completions
-  .create({
-    model: model,
-    messages: messages,
-  })
-  .then((response) => {
-    const translatedText = response.choices[0].message.content;
-    console.log(translatedText);
-
-    const outputFile = "output.md";
-    fs.writeFileSync(outputFile, translatedText, "utf-8");
-    const markdownContent = fs.readFileSync(outputFile, "utf8");
-
-    const { data, content } = matter(markdownContent);
-
-    const match = content.match(/^#\s+(.+)$/m);
-    const title = match ? match[1] : "";
-
-    const newMarkdownContent = `---
-title: "${title || ""}"
-summary: ""
+  const messages = [
+    {
+      role: "system",
+      content:
+        "Please remember to stimulus without translation first, then translate the following Markdown text into Chinese and return it to me in Markdown code format", // 翻译的条件
+    },
+    { role: "user", content: content },
+  ];
+  openai.chat.completions
+    .create({
+      model: "gpt-3.5-turbo",
+      messages: messages,
+    })
+    .then((response) => {
+      const translatedContent = response.choices[0].message.content;
+      const newMarkdownContent = `---
 permalink: ${data.permalink || ""}
-eleventyNavigation:
-  key: ${title || ""}
-  parent: Handbook
-  order: ${data.order || ""}
+order: ${data.order || ""}
 ---
-  ${content}
-  `;
 
-    fs.writeFileSync("second-md-file.md", newMarkdownContent, "utf8");
-  })
-  .catch((error) => console.error("Error:", error));
+${translatedContent}`;
+      fs.writeFileSync("translated_" + markdownFile, newMarkdownContent, "utf8");
+    })
+    .catch((error) => console.error("Error:", error));
+}
