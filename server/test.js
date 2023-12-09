95e383532287290618865f24e3c3eb253eb73a30
firstmeet <2022742378@qq.com>
2023-12-09 16:13:18 +0800

    1

--- server/test.js
+++ server/test.js
@@ -1,8 +1,18 @@
 const { execSync } = require('child_process');
 const path = require('path');
+const translate = require('./plugin/openai-test');
 
-// const fileUrl = process.cwd();
-// const fileUrl = path.resolve(process.cwd(), '../docs');
+function patchParse(patchStr) {
+  const fileDataArr = [];
+  const relativeUrl = patchStr.split('+++ b/docs/')[1].split('\n')[0];
+  const fileDataItem = {
+    fileName: relativeUrl.split('/')[1].split('.md')[0],
+    fileCategory: relativeUrl.split('/')[0],
+  };
+  fileDataArr.push(fileDataItem);
+  return fileDataArr;
+}
+
 const config = {
   cwd: '../',
 };
@@ -10,15 +20,16 @@ const patchStr = execSync(
   'git diff newDoc remotes/targetdoc/main -- docs',
   config,
 ).toString();
-const relativeUrl = patchStr.split('+++ b/docs/')[1].split('\n')[0];
-console.log(relativeUrl);
-// const absoluteInputUrl = path.resolve(fileUrl, relativeUrl);
-// const outputUrl = absoluteInputUrl
-//   .replace('docs', 'docs_chs')
-//   .split('/')
-//   .slice(0, -1)
-//   .join('/');
 
-/**
- * @param {string} relativeUrl 待翻译文件的相对路径
- */
+if (!patchStr) {
+  console.log('无更新');
+} else {
+  execSync(
+    'git diff newDoc remotes/targetdoc/main -- docs > ./server/diff.patch',
+    config,
+  );
+  execSync('git apply ./server/diff.patch', config);
+  const fileDataArr = patchParse(patchStr);
+
+  translate(fileDataArr);
+}
